719b32bddbe179e1b8273270d3abf429c5db024a
Vitalij Kudresov <kudresov@gmail.com>
2014-11-30 11:52:51 +0000

    added validation and states to the main screen

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,30 @@
 angular.module('homeRemovalApp')
   .controller('MainCtrl', function ($scope, removalItemService) {
     $scope.removalItemService = removalItemService;
+    $scope.state = 'init';
+
+    $scope.clearSelection = function() {
+      $scope.selectedItem = null;
+      $scope.state='init';
+    };
+
     $scope.addItem = function(item) {
       removalItemService.addItem(item);
-      $scope.selectedItem = null;
+      $scope.clearSelection();
+    };
+
+    $scope.updateItem = function(item) {
+      removalItemService.updateItem(item);
+      $scope.clearSelection();
+    };
+
+    $scope.deleteItem = function(item) {
+      removalItemService.deleteItem(item);
+      $scope.clearSelection();
     };
 
     $scope.selectItem = function(item) {
-      $scope.selectedItem = item;
+      $scope.selectedItem = angular.copy(item);
+      $scope.state='edit';
     };
   });
--- app/scripts/services/removalitemservice.js
+++ app/scripts/services/removalitemservice.js
@@ -9,22 +9,38 @@
  */
 angular.module('homeRemovalApp')
   .service('removalItemService', function () {
-    this.items = [
-      {room: 'One', content: 'books', weight: 25, isFragile: false}, 
-      {room: 'Two', content: 'rocks', weight: 32, isFragile: false}, 
-      {room: 'Three', content: 'pc', weight: 4, isFragile: true}, 
-      {room: 'Four', content: 'utencils', weight: 3, isFragile: false}, 
-      {room: 'Five', content: 'clothes', weight: 5, isFragile: false},
-      {room: 'Six', content: 'shoes', weight: 5, isFragile: false},
-      {room: 'Seven', content: 'belts', weight: 1, isFragile: false}];
+    var id = 0;
+    this.items = [];
+    // this.items = [
+    //   {room: 'One', content: 'books', weight: 25, isFragile: false}, 
+    //   {room: 'Two', content: 'rocks', weight: 32, isFragile: false}, 
+    //   {room: 'Three', content: 'pc', weight: 4, isFragile: true}, 
+    //   {room: 'Four', content: 'utencils', weight: 3, isFragile: false}, 
+    //   {room: 'Five', content: 'clothes', weight: 5, isFragile: false},
+    //   {room: 'Six', content: 'shoes', weight: 5, isFragile: false},
+    //   {room: 'Seven', content: 'belts', weight: 1, isFragile: false}];
 
     this.addItem = function(item) {
       console.log('Removal Item Service: Item added');
+      item.id = id;
+      id+=1;
       this.items.push(item);
     };
 
+    this.updateItem = function(item) {
+      this.deleteItem(item);
+      this.addItem(item);
+    };
+
     this.deleteItem = function(item) {
-      var itemToRemoveIndex = this.items.indexOf(item);
+      var itemToRemoveIndex = -1;
+      for (var i = 0, len = this.items.length; i < len; i++) {
+        if(this.items[i].id === item.id) {
+          itemToRemoveIndex = i;
+          break;
+        }
+      }
+      
       if (itemToRemoveIndex > -1) {
         this.items.splice(itemToRemoveIndex, 1);
       }
