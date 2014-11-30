59a757894ca48f29277276ff5fae173d20ba028e
Vitalij Kudresov <kudresov@gmail.com>
2014-11-30 17:26:16 +0000

    added service tests

--- app/scripts/services/removalitemservice.js
+++ app/scripts/services/removalitemservice.js
@@ -11,14 +11,6 @@ angular.module('homeRemovalApp')
   .service('removalItemService', function () {
     var id = 0;
     this.items = [];
-    // this.items = [
-    //   {room: 'One', content: 'books', weight: 25, isFragile: false}, 
-    //   {room: 'Two', content: 'rocks', weight: 32, isFragile: false}, 
-    //   {room: 'Three', content: 'pc', weight: 4, isFragile: true}, 
-    //   {room: 'Four', content: 'utencils', weight: 3, isFragile: false}, 
-    //   {room: 'Five', content: 'clothes', weight: 5, isFragile: false},
-    //   {room: 'Six', content: 'shoes', weight: 5, isFragile: false},
-    //   {room: 'Seven', content: 'belts', weight: 1, isFragile: false}];
 
     this.addItem = function(item) {
       console.log('Removal Item Service: Item added');
@@ -29,7 +21,7 @@ angular.module('homeRemovalApp')
 
     this.updateItem = function(item) {
       this.deleteItem(item);
-      this.addItem(item);
+      this.items.push(item);
     };
 
     this.deleteItem = function(item) {
@@ -40,7 +32,7 @@ angular.module('homeRemovalApp')
           break;
         }
       }
-      
+
       if (itemToRemoveIndex > -1) {
         this.items.splice(itemToRemoveIndex, 1);
       }
--- test/spec/services/removalitemservice.js
+++ test/spec/services/removalitemservice.js
@@ -11,8 +11,137 @@ describe('Service: removalItemService', function () {
     removalItemService = _removalItemService_;
   }));
 
-  it('should do something', function () {
+  it('should be something', function () {
     expect(!!removalItemService).toBe(true);
   });
 
+  it('should be initialised with no item', function() {
+    expect(removalItemService.items.length).toEqual(0);
+  });
+
+  describe('adding item', function() {
+
+    describe('one time', function() {
+
+      it('should only add that one item', function() {
+        var item = {room: 'Living Room', content: 'books', weight: 5, isFragile: false};
+
+        removalItemService.addItem(item);
+
+        expect(removalItemService.items.length).toEqual(1);
+        expect(removalItemService.items[0]).toEqual(item);
+      });
+    });
+
+    describe('multiple items', function() {
+      
+      it('should increment id by one', function() {
+        var books = {room: 'Living Room', content: 'books', weight: 5, isFragile: false};
+        var pc = {room: 'Living Room', content: 'pc', weight: 2, isFragile: true};
+        
+        removalItemService.addItem(books);
+        expect(removalItemService.items[0].id).toEqual(0);
+
+        removalItemService.addItem(pc);
+        expect(removalItemService.items[1].id).toEqual(1);
+      });
+
+      it('should add exactly same items', function() {
+        var item1 = {room: 'Living Room', content: 'books', weight: 5, isFragile: false};
+        var item2 = {room: 'Living Room', content: 'books', weight: 5, isFragile: false};
+
+        removalItemService.addItem(item1);
+        removalItemService.addItem(item2);
+
+        expect(removalItemService.items.length).toEqual(2);
+      });
+    });
+  });
+
+  describe('deleting item', function() {
+    
+    describe('with id does not exist', function() {
+
+      it('should NOT delete any items', function() {
+        var books = {room: 'Living Room', content: 'books', weight: 5, isFragile: false};
+        var pc = {room: 'Living Room', content: 'pc', weight: 2, isFragile: true};
+        var unicorn = {room: 'Unicorn Stables', content: 'unicorn', weight: 200, isFragile: true, id: 5};
+
+        removalItemService.addItem(books);
+        removalItemService.addItem(pc);
+
+        removalItemService.deleteItem(unicorn);
+
+        expect(removalItemService.items.length).toEqual(2);
+      });
+    });    
+
+    describe('which does not exist', function() {
+
+      it('should NOT delete any items', function() {
+        var books = {room: 'Living Room', content: 'books', weight: 5, isFragile: false};
+        var pc = {room: 'Living Room', content: 'pc', weight: 2, isFragile: true};
+        var unicorn = {room: 'Unicorn Stables', content: 'unicorn', weight: 200, isFragile: true};
+
+        removalItemService.addItem(books);
+        removalItemService.addItem(pc);
+
+        removalItemService.deleteItem(unicorn);
+
+        expect(removalItemService.items.length).toEqual(2);
+      });
+    });
+
+    describe('which exists', function() {
+
+      it('should remove it from the service', function() {
+        var books = {room: 'Living Room', content: 'books', weight: 5, isFragile: false};
+
+        removalItemService.addItem(books);
+        var itemToDelete = removalItemService.items[0];
+        removalItemService.deleteItem(itemToDelete);
+
+        expect(removalItemService.items.length).toEqual(0);
+      });
+    });
+  });
+
+  describe('update item', function() {
+
+    it('should update all properties', function() {
+      var item = {room: 'one', content: 'one', weight: 1, isFragile: true};
+
+      removalItemService.addItem(item);
+      var itemToUpdate = removalItemService.items[0];
+      itemToUpdate.room = 'two';
+      itemToUpdate.content = 'two';
+      itemToUpdate.weight = 2;
+      itemToUpdate.isFragile = false;
+
+      removalItemService.updateItem(itemToUpdate);
+      var updatedItem = removalItemService.items[0];
+
+      expect(updatedItem.room).toEqual('two');
+      expect(updatedItem.content).toEqual('two');
+      expect(updatedItem.weight).toEqual(2);
+      expect(updatedItem.isFragile).toEqual(false);
+    });
+
+    it('should not change id', function() {
+      var item = {room: 'one', content: 'one', weight: 1, isFragile: true};
+
+      removalItemService.addItem(item);
+      var originalId = removalItemService.items[0].id;
+      var itemToUpdate = removalItemService.items[0];
+      itemToUpdate.room = 'two';
+      itemToUpdate.content = 'two';
+      itemToUpdate.weight = 2;
+      itemToUpdate.isFragile = false;
+
+      removalItemService.updateItem(itemToUpdate);
+      var newId = removalItemService.items[0].id;
+
+      expect(originalId).toEqual(newId);
+    });
+  });
 });
